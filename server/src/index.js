67acd7eafe76dc07bb98971355adc8676f1f7282
beema-1988 <you@example.com>
2025-03-28 00:25:49 -0700

    Your commit -1

--- server/src/index.js
+++ server/src/index.js
@@ -30,7 +30,7 @@ app.use('/api/payment',paymentRoute)
 
 
 
-// const PORT=process.env.PORT||3000
+const PORT=process.env.PORT
 app.listen(PORT,()=>{
     console.log(`server listening to port ${PORT}`)
 })
