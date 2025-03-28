7a733565544d503bff173496d6d7ca866bcb05cf
beema-1988 <you@example.com>
2025-03-28 04:23:00 -0700

    Fixed 404 error and updated routing

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,18 @@
 const express=require('express')
 const cookieparser=require('cookie-parser')
-const app=express()
+// const app=express()
 const cors=require("cors")
-require('dotenv').config()
+const dotenv=require('dotenv')
+// require('dotenv').config()
 const connectDB=require('../src/config/db')
 const userRoute=require('../src/Routes/userroute')
 const adminRoute=require('../src/Routes/adminroute')
 const eventRoute=require('../src/Routes/eventRoute')
 const ticketRoute=require('../src/Routes/ticketroute')
 const paymentRoute=require('../src/Routes/paymentRoute')
+
+dotenv.config()
+const app=express()
 connectDB()
 
 
@@ -30,7 +34,12 @@ app.use('/api/payment',paymentRoute)
 
 
 
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = app; // Exports the app to be used by Vercel
+} else {
 app.listen(PORT,()=>{
     console.log(`server listening to port ${PORT}`)
 })
+}
